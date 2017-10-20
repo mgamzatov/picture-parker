d5d3362457501f9a8a54985a8e4714c927814219
EpicWin332 <gamzatovmagomed@mail.ru>
2017-10-21 02:14:38 +0300

    remove test label

--- src/main/java/ru/bugdealers/pictureparker/PictureParkerApplication.java
+++ src/main/java/ru/bugdealers/pictureparker/PictureParkerApplication.java
@@ -1,12 +1,10 @@
 package ru.bugdealers.pictureparker;
 
 import okhttp3.OkHttpClient;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import ru.bugdealers.pictureparker.bot.VkBotRunner;
-// test
+
 @SpringBootApplication
 public class PictureParkerApplication {
 
