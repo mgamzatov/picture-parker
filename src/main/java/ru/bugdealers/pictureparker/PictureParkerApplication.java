681237c74621b65749070ba0c24f52d6d83d205a
YRashid <ry1312@ya.ru>
2017-10-20 20:25:11 +0300

    test

--- src/main/java/ru/bugdealers/pictureparker/PictureParkerApplication.java
+++ src/main/java/ru/bugdealers/pictureparker/PictureParkerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import ru.bugdealers.pictureparker.bot.VkBotRunner;
-
+// test
 @SpringBootApplication
 public class PictureParkerApplication {
 
